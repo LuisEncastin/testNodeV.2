8d07bb971bd992cf8556f7bca06f3cbb268e62ef
LuisEncastin <luisencastin@gmail>
2022-02-02 18:46:14 -0600

    Chaining Middleware to Create a Time Server

--- myApp.js
+++ myApp.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 app.use(function(req, res, next) {
   console.log(req.method + " " + req.path + " - " + req.ip)
   next()
-})
+});
 
 // --> 11)  Mount the body-parser middleware  here
 
@@ -60,6 +60,12 @@ app.get("/json", function(req, res) {
 
 /** 8) Chaining middleware. A Time server */
 
+app.get('/now', function(req, res, next){
+  req.time = new Date().toString();
+  next();
+}, function(req, res){
+  res.send({time: req.time});
+});
 
 /** 9)  Get input from client - Route parameters */
 
