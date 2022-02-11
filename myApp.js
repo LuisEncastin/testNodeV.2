903a139be288b63aa55bd185d56d8d96211a2fee
LuisEncastin <luisencastin@gmail>
2022-02-10 18:26:50 -0600

    Adding changes to Use body-parser to Parse POST Requests. Modification

--- myApp.js
+++ myApp.js
@@ -12,11 +12,9 @@ app.use(function(req, res, next) {
 
 // --> 11)  Mount the body-parser middleware  here
 
-app.use(function(req, res, next) {
-  bodyParser.urlencoded({extended: false});
-  console.log(bodyParser);
-  next();
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
 
 /** 1) Meet the node console. */
 bGround.log("Hello World");
